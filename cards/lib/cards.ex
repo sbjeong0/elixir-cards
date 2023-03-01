c2041b398e0636af6f7318e95cdb1602fa4a7b2c
정수빈 <jeongsubin@jeongsubin-ui-MacBookPro.local>
2023-03-01 21:25:58 +0900

    section 3

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,7 +1,15 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
   def hello do
     "hi there!"
   end
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards.
+  """
 
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -16,11 +24,29 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
-  @spec deal(any, integer) :: {list, list}
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+    ## Examples
+        iex> deck = Cards.create_deck
+        iex> {hand, _} = Cards.deal(deck, 1)
+        iex> hand
+        ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -3,6 +3,20 @@ defmodule CardsTest do
   doctest Cards
 
   test "greets the world" do
-    assert Cards.hello() == :world
+    assert Cards.hello() == "hi there!"
+  end
+
+  test "the truth" do
+    assert 1+1 == 2
+  end
+
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
   end
 end
